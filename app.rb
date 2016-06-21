ab4068665fca9b525d5e29ead4788d62a9554b01
Leigh Stauffer <leigh.stauffer@gmail.com>
2016-06-21 20:05:22 +0000

    Here's a really crappy rainbow

--- app.rb
+++ app.rb
@@ -21,6 +21,11 @@ class App < Sinatra::Base
     erb :youtube
   end
 
+  get '/rainbow' do
+    erb :rainbow
+
+  end
+
   get '/reddit' do
     # TODO: we can probably get the listings with something like:
     # JSON.parse(RestClient.get('http://reddit.com/.json'))
